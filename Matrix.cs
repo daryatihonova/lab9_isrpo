fbd4f69ce0a6e30d2c459cb6999fe16702e4b477
Darya Tikhonova <daryatikhonova@bk.ru>
2024-11-03 23:32:57 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_9
+{
+    public class Matrix
+    {
+        private double[,] matrix; // Закрытое поле - матрица вещественных чисел
+
+        public int Rows { get; } // Свойство для определения количества строк матрицы
+        public int Cols { get; } // Свойство для определения количества столбцов матрицы
+
+        // Индексатор для доступа к элементам поля-массиву
+        public double this[int i, int j]
+        {
+            get { return matrix[i, j]; }
+            set { matrix[i, j] = value; }
+        }
+
+        /// <summary>
+        /// Конструктор с двумя параметрами - количество строк, количество столбцов матрицы
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="cols"></param>
+        public Matrix(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            matrix = new double[Rows, Cols];
+        }
+
+        /// <summary>
+        /// Конструктор с одним параметром - матрица вещественных чисел
+        /// </summary>
+        /// <param name="array"></param>
+        public Matrix(double[,] array)
+        {
+            Rows = array.GetLength(0);
+            Cols = array.GetLength(1);
+            matrix = array;
+        }
+
+        /// <summary>
+        /// Метод для вычисления количества элементов матрицы, больших заданного числа
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public int CountElementsGreaterThan(double number)
+        {
+            int count = 0;
+            foreach (var element in matrix)
+            {
+                if (element > number)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Метод для вычисления количества элементов матрицы, больших заданного числа 
+        /// и расположенных в столбцах с номерами кратными заданному целому числу
+        /// </summary>
+        /// <param name="number">число</param>
+        /// <param name="divisor">делитель</param>
+        /// <returns></returns>
+        public int CountElementsGreaterThanMultiple(double number, int divisor)
+        {
+            int count = 0;
+            for (int j = 0; j < Cols; j++)
+            {
+                if (j % divisor == 0)
+                {
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        if (matrix[i, j] > number)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using lab_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание матрицы 3x3
+        Matrix matrix = new Matrix(3, 3);
+
+        // Заполнение матрицы значениями
+        matrix[0, 0] = 1.5;
+        matrix[0, 1] = 2.5;
+        matrix[0, 2] = 3.5;
+        matrix[1, 0] = 4.5;
+        matrix[1, 1] = 5.5;
+        matrix[1, 2] = 6.5;
+        matrix[2, 0] = 7.5;
+        matrix[2, 1] = 8.5;
+        matrix[2, 2] = 9.5;
+
+        // Вывод матрицы на экран
+        Console.WriteLine("Матрица:");
+        for (int i = 0; i < matrix.Rows; i++)
+        {
+            for (int j = 0; j < matrix.Cols; j++)
+            {
+                Console.Write(matrix[i, j] + "  ");
+            }
+            Console.WriteLine();
+        }
+
+        // Подсчет элементов больше заданного числа
+        double numberToCompare = 5.0;
+        int countGreaterThan = matrix.CountElementsGreaterThan(numberToCompare);
+        Console.WriteLine($"Количество элементов больше {numberToCompare}: {countGreaterThan}");
+
+        // Подсчет элементов больше заданного числа в столбцах с номерами кратными заданному делителю
+        int divisor = 1; // кратные единице
+        int countGreaterThanMultiple = matrix.CountElementsGreaterThanMultiple(numberToCompare, divisor);
+        Console.WriteLine($"Количество элементов больше {numberToCompare} в столбцах с номерами кратными {divisor}: {countGreaterThanMultiple}");
+        // Подсчет элементов больше заданного числа в четных столбцах
+        divisor = 2; // кратные двум
+        countGreaterThanMultiple = matrix.CountElementsGreaterThanMultiple(numberToCompare, divisor);
+        Console.WriteLine($"Количество элементов больше {numberToCompare} в четных столбцах: {countGreaterThanMultiple}");
+    }
+}
